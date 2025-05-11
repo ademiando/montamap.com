5b4b537abfe2d6c4390bf85252419fbd3f4be1f0
ademiando <ademiandoo@gmail.com>
2025-05-12 00:29:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -280,7 +280,7 @@ async function renderMountains() {
 
          
 
-{`<img src="https://openweathermap.org/img/wn/${weather.icon}.png" alt="${weather.weather}" style="vertical-align:middle; width: 28px; height: 28px;" />`} ${weather.temperature}<br />
+${`<img src="https://openweathermap.org/img/wn/${weather.icon}.png" alt="${weather.weather}" style="vertical-align:middle; width: 28px; height: 28px;" />`} ${weather.temperature}<br />
 ${weather.weather}
 
 

71af931fd04fd552fd18f8ba573ae0a6b23e5aa7
ademiando <ademiandoo@gmail.com>
2025-05-17 12:54:01 +0800

    Update script.js

--- prau/script.js
+++ prau/script.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // === script.js untuk halaman Gunung Prau ===
 
-// TAB SWITCHING const tabs = document.querySelectorAll('.tab'); const contents = document.querySelectorAll('.tab-content');
+// TAB SWITCHING 
+const tabs = document.querySelectorAll('.tab'); const contents = document.querySelectorAll('.tab-content');
 
 tabs.forEach(tab => { tab.addEventListener('click', () => { tabs.forEach(t => t.classList.remove('active')); contents.forEach(c => c.classList.remove('active')); tab.classList.add('active'); document.getElementById(tab.dataset.tab).classList.add('active'); }); });
 

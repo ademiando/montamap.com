94656767476b27204558b5e9979f18444cfbb9e0
ademiando <ademiandoo@gmail.com>
2025-06-04 21:47:04 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  
+  MAPBOX_TOKEN:         "pk.eyJ1IjoiYWRlbWlhbmRvIiwiYSI6ImNtYXF1YWx6NjAzdncya3B0MDc5cjhnOTkifQ.RhVpan3rfXY0fiix3HMszg",
+
+  OPENWEATHER_API_KEY:  "3187c49861f858e524980ea8dd0d43c6",
+
+  SUPABASE_URL:         "https://wmutwaurieyuujbwvzav.supabase.co",
+
+  SUPABASE_KEY:         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtdXR3YXVyaWV5dXVqYnd2emF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5NDk1MTcsImV4cCI6MjA2NDUyNTUxN30.hjdpU8Vo7b5x-0VKUMdoe9Qbm189mw7PMvyVnACD7eY"
+};
